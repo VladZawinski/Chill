9eca7c29bd3fe3596ba1331071110a295287ac13
Thihakaungset3 <Kaungset138@icloud.com>
2020-04-19 21:08:29 +0630

    add filter by page

--- modules/pornhub.js
+++ modules/pornhub.js
@@ -1,13 +1,14 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-let url = "https://www.pornhub.com/pornstars/"
-
+let url = "https://www.pornhub.com/pornstars"
 
 const fetchPornstars = function(page){
      return new Promise((resolve,reject) => {
+     
+     let _url = createUrlWithPage(page)
 
-          fetch(url)
+     fetch(_url)
      .then(res => res.text())
      .then(body => {
           const $ = cheerio.load(body)
@@ -15,21 +16,25 @@ const fetchPornstars = function(page){
           const posts = $('#popularPornstars li')
 
           const result = [];
-
+          
           for(let i = 0;i < posts.length;i++){
                const post = $(posts[i])
-               const img = post.find('div img').attr('data-thumb_url')
-               const name = post.find('div a').attr("data-mxptext")
-               const rank = post.find('div .rank_number').text().trim()
-               const url = post.find('.js-mxp').attr('href')
                
+               if(post.text().trim() !== 'Remove Ads'){
+                    const img = post.find('div img').attr('data-thumb_url')
+                    const name = post.find('div a').attr("data-mxptext")
+                    const rank = post.find('div .rank_number').text().trim()
+                    const url = post.find('.js-mxp').attr('href')
+                    
+                    
+                    result.push({
+                         name : name,
+                         image : img,
+                         rank : rank,
+                         url : url
+                    })
+               }
                
-               result.push({
-                    name : name,
-                    image : img,
-                    rank : rank,
-                    url : url
-               })
           }
 
           resolve(result)
@@ -37,6 +42,10 @@ const fetchPornstars = function(page){
           }).catch(e => reject(e))
 
      });
+}
+
+function createUrlWithPage(page) {
+     return `${url}?page=${page}`
 }
 
 module.exports = fetchPornstars
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 1338
 const pornhub = require('./modules/pornhub')
 
 app.get('/pornstars/:page', (req, res) => {
-     pornhub(1)
+     let page = req.params.page || 1
+     
+     pornhub(page)
           .then(result => res.send(result))
           .catch(e => console.log(e))
 });
